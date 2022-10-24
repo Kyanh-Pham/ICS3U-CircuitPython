895b2dfe50595a7646a653be13050a0de8bc0676
Kyanh-Pham <113041431+Kyanh-Pham@users.noreply.github.com>
2022-10-24 11:11:58 -0400

    Create code.py

---
+++ code.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Created by: Kyanh Pham
+# Created on: Oct 2022
+# This program is the "Space Aliens" program on the PyBadge
+
+import ugame
+import stage
+
+def game_scene():
+    # This function is the main game game_scene
+
+    print("\n\n\n")
+    print("Hello, world!")
+
+    print("\nDone.")
+
+    # repeat forever, game loop
+    while True:
+        pass # placeholder
+
+if __name__ == "__main__":
+    game_scene()
