91e1a78a20fae7995ef546c1a25e81a26db5402a
Kyanh-Pham <113041431+Kyanh-Pham@users.noreply.github.com>
2022-10-31 10:52:01 -0400

    added sprite

--- code.py
+++ code.py
@@ -9,19 +9,24 @@ import ugame
 
 
 def game_scene():
-    # This function is the main game game_scene
+   # This function is the main game game_scene
 
-    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
-    background = stage.Grid(image_bank_background, 10, 8)
+   image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+   image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
 
-    game = stage.Stage(ugame.display, 60)
-    game.layers = [background]
-    game.render_block()
+   background = stage.Grid(image_bank_background, 10, 8)
 
-    # repeat forever, game loop
-    while True:
-        pass  # placeholder
+   ship = stage.Sprite(image_bank_sprites, 5, 75 ,66)
+
+   game = stage.Stage(ugame.display, 60)
+   game.layers = [ship] + [background]
+   game.render_block()
+
+   # repeat forever, game loop
+   while True:
+       game.render_sprites([ship])
+       game.tick()
 
 
 if __name__ == "__main__":
-    game_scene()
+   game_scene()
