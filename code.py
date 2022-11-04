399506a745ae0ea5f1c30e00f8bc0753eeb406de
Kyanh-Pham <113041431+Kyanh-Pham@users.noreply.github.com>
2022-11-04 10:48:01 -0400

    fixed errors

--- code.py
+++ code.py
@@ -16,9 +16,11 @@ def game_scene():
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
     image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
 
-    background = stage.Grid(image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y)
+    background = stage.Grid(
+        image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y)
 
-    ship = stage.Sprite(image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE))
+    ship = stage.Sprite(
+        image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE))
 
     game = stage.Stage(ugame.display, constants.FPS)
     game.layers = [ship] + [background]
@@ -44,13 +46,14 @@ def game_scene():
 
         if keys & ugame.K_LEFT != 0:
             if ship.x > 0:
-                ship.move((ship.x-constants.SPRITE_MOVEMENT_SPEED), ship.y)
+                ship.move((ship.x - constants.SPRITE_MOVEMENT_SPEED), ship.y)
             else:
                 ship.move(0, ship.y)
         if keys & ugame.K_UP:
             pass
         if keys & ugame.K_DOWN:
             pass
+
 
 if __name__ == "__main__":
     game_scene()
