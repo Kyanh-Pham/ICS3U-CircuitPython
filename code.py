ba67ffacdc601ebb50382792a121d07e81caf4f9
Kyanh-Pham <113041431+Kyanh-Pham@users.noreply.github.com>
2022-11-04 10:59:17 -0400

    fixed spacing again

--- code.py
+++ code.py
@@ -17,11 +17,11 @@ def game_scene():
 
     background = stage.Grid(
         image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y
-        )
+    )
 
     ship = stage.Sprite(
         image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE)
-        )
+    )
 
     game = stage.Stage(ugame.display, constants.FPS)
     game.layers = [ship] + [background]
@@ -39,13 +39,13 @@ def game_scene():
             pass
         if keys & ugame.K_SELECT:
             pass
-        if keys & ugame.K_RIGHT != 0:
+        if keys & ugame.K_RIGHT 0:
             if ship.x <= (constants.SCREEN_X - constants.SPRITE_SIZE):
                 ship.move((ship.x + constants.SPRITE_MOVEMENT_SPEED), ship.y)
             else:
                 ship.move((constants.SCREEN_X - constants.SPRITE_SIZE), ship.y)
 
-        if keys & ugame.K_LEFT != 0:
+        if keys & ugame.K_LEFT 0:
             if ship.x >= 0:
                 ship.move((ship.x - constants.SPRITE_MOVEMENT_SPEED), ship.y)
             else:
