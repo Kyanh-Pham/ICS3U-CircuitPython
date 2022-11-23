6ae814558baa5dc038cbf8a455555c50843c9159
Kyanh-Pham <113041431+Kyanh-Pham@users.noreply.github.com>
2022-11-23 10:45:16 -0500

    linting fix

--- code.py
+++ code.py
@@ -150,6 +150,7 @@ def game_scene():
 
     # get sound ready
     pew_sound = open("pew.wav", "rb")
+    boom_sound = open("boom.wav", "rb")
     sound = ugame.audio
     sound.stop()
     sound.mute(False)
@@ -267,24 +268,34 @@ def game_scene():
                     )
                     show_alien()
 
+        # each frame check if any of the lasers are touching any of the aliens
         for laser_number in range(len(lasers)):
             if lasers[laser_number].x > 0:
                 for alien_number in range(len(aliens)):
                     if aliens[alien_number].x > 0:
-                        if stage.collide(lasers[laser_number].x + 6, lasers[laser_number].y + 2,
-                         lasers[laser_number].x + 11, lasers[laser_number].y + 12,
-                         aliens[alien_number].x + 1, aliens[alien_number].y,
-                         aliens[alien_number].x + 15, aliens[alien_number].y, + 15):
+                        if stage.collide(
+                            lasers[laser_number].x + 6,
+                            lasers[laser_number].y + 2,
+                            lasers[laser_number].x + 11,
+                            lasers[laser_number].y + 12,
+                            aliens[alien_number].x + 1,
+                            aliens[alien_number].y,
+                            aliens[alien_number].x + 15,
+                            aliens[alien_number].y + 15,
+                        ):
                             # you hit an alien
-                            aliens[alien_number].move(constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y)
-                            lasers[laser_number].move(constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y)
+                            aliens[alien_number].move(
+                                constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y
+                            )
+                            lasers[laser_number].move(
+                                constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y
+                            )
                             sound.stop()
                             sound.play(boom_sound)
                             show_alien()
                             show_alien()
                             alien_count = alien_count + 1
                             score = score + 1
-
 
         game.render_sprites(aliens + lasers + [ship])
         game.tick()
